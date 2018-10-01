9a64d7466f9f603174e7015312fe6139f98fe864
willw00 <wweiss00@gmail.com>
2018-09-30 21:10:06 -0500

    Slider bar updates time readout

--- app/src/main/java/com/example/willweiss/intervaltracker/MainActivity.kt
+++ app/src/main/java/com/example/willweiss/intervaltracker/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.willweiss.intervaltracker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
+
+    override fun onProgressChanged(seekBar: SeekBar, progress: Int,
+                                   fromUser: Boolean) {
+        pickedTime.text = progress.toString() + "s"
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar) {
+        // called when tracking the seekbar is started
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar) {
+        // called when tracking the seekbar is stopped
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        timePicker.setOnSeekBarChangeListener(this)
     }
 }
