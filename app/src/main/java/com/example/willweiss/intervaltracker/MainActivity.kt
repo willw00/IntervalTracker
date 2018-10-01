e5bd66606a2266ec2c0339aba4ac674706d34419
willw00 <wweiss00@gmail.com>
2018-09-30 21:18:59 -0500

    Increment progress bar

--- app/src/main/java/com/example/willweiss/intervaltracker/MainActivity.kt
+++ app/src/main/java/com/example/willweiss/intervaltracker/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.willweiss.intervaltracker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,17 +13,18 @@ class MainActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener {
         pickedTime.text = progress.toString() + "s"
     }
 
-    override fun onStartTrackingTouch(seekBar: SeekBar) {
-        // called when tracking the seekbar is started
-    }
+    override fun onStartTrackingTouch(seekBar: SeekBar) {}
 
-    override fun onStopTrackingTouch(seekBar: SeekBar) {
-        // called when tracking the seekbar is stopped
-    }
+    override fun onStopTrackingTouch(seekBar: SeekBar) {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         timePicker.setOnSeekBarChangeListener(this)
+        pickedTime.text = timePicker.progress.toString() + "s"
+    }
+
+    fun incrementProgressBar(view: View) {
+        timerBar.incrementProgressBy(10)
     }
 }
