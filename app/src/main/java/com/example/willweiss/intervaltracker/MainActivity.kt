3d1d4981dc79bf013baa28d6fdbcb4643a8573ab
willw00 <wweiss00@gmail.com>
2018-10-27 20:23:17 -0500

    Better closer warmer

--- app/src/main/java/com/example/willweiss/intervaltracker/MainActivity.kt
+++ app/src/main/java/com/example/willweiss/intervaltracker/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.willweiss.intervaltracker
 
+import android.content.ClipData
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBar
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import com.example.willweiss.intervaltracker.components.ProgressBarUpdatingCountDownTimer
@@ -21,9 +23,15 @@ class MainActivity : AppCompatActivity() {
 
     private val UPDATE_PROGRESS_BAR: Int = 1
 
-    private val intervalSet = IntervalSet(listOf(
-            Interval("First!", 10),
-            Interval("Second!", 10)))
+    private val intervalSet = IntervalSet("Set1", listOf(
+            Interval("First!", 5),
+            Interval("Second!", 5)))
+
+    private val intervalSet2 = IntervalSet("Set2", listOf(
+            Interval("First2!", 5),
+            Interval("Second2!", 5)))
+
+    private val intervalSets = mapOf(0 to intervalSet, 1 to intervalSet2)
 
     val progressBarHandler = Handler() {message ->
         if (message.what == UPDATE_PROGRESS_BAR) {
@@ -55,8 +63,14 @@ class MainActivity : AppCompatActivity() {
 
         navDrawer.setNavigationItemSelectedListener { menuItem ->
             menuItem.isChecked = true
+
             drawer_layout.closeDrawers()
             true
+        }
+
+        intervalSets.forEach {it ->
+            val newItem = navDrawer.menu.add(Menu.NONE, it.key, it.key, it.value.name)
+            println(newItem)
         }
 
         timePickerListener = TimePickerChangeListener(pickedTime)
@@ -64,6 +78,16 @@ class MainActivity : AppCompatActivity() {
         timePicker.setOnSeekBarChangeListener(timePickerListener)
 
         pickedTime.text = timePicker.progress.toString() + "s"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menu?.clear()
+//        menuInflater.inflate(R.menu.drawer_view, menu)
+//        menu?.add(0, 0, Menu.NONE, "Option1")
+
+        println(menu?.size())
+        val intervals =
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
